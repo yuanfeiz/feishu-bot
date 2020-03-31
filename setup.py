42dcd87811a1f7068acaedd7db8617ffc4f75cf7
Yuanfei Zhu <yuanfei@satori.works>
2020-03-31 22:32:10 +0800

    bump to v0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="feishu_bot",  # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Yuanefi Zhu",
     author_email="abovemoon@outlook.com",
     description="SDK for interacting with Feishu(previously Lark)",
